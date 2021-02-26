530be56fcf2a1bcf99fba2683f534453b2c571ed
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-02-26 14:51:38 +0100

    pwa code added

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,11 +1,13 @@
-  
 export function register() {
-
-  let swDev = `${process.env.PUBLIC_URL}/service-worker.js`
-  navigator.serviceWorker.register(swDev).then((result) => {
-    console.log("Result", result);
-  })
+  if ("serviceWorker" in navigator) {
+    // console.log(`${process.env.PUBLIC_URL}/CustomSW.js`)
+    navigator.serviceWorker
+      .register(`${process.env.PUBLIC_URL}/service-worker.js`)
+      .then(function (registration) {
+        console.log("Registration successful, scope is:", registration.scope);
+      })
+      .catch(function (error) {
+        console.log("Service worker registration failed, error:", error);
+      });
+  }
 }
-
-
-
