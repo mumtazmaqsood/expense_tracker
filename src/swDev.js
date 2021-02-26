190b9353dc70009cbb10d187a86aae0e7fc1d4db
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-02-26 08:44:08 +0100

    app.js changes pwa

--- src/swDev.js
+++ src/swDev.js
@@ -1,7 +1,9 @@
 export default function swDev() {
-    let swDev = `${process.env.PUBLIC_URL}/sw.js`;
-    navigator.serviceWorker.register(swDev).then(function(result) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', result);
-      })
+
+  let swDev = `${process.env.PUBLIC_URL}/sw.js`
+  navigator.serviceWorker.register(swDev).then((result) => {
+    console.log("Result", result);
+  })
+
+
 }
